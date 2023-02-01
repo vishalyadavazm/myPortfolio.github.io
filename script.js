6ff6786ad89f04ced5bb5da25c0dc283311582cd
vishalyadavazm <93594891+vishalyadavazm@users.noreply.github.com>
2023-02-01 20:10:39 +0530

    portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-var humburger=document.querySelector(".hamb")
-var links=document.querySelector(".nav-list")
-var navlist=document.querySelector(".nav-list li")
-
-humburger.addEventListener("click ",function(){
-this.classList.toggle ("click ");
-navlist.classList.toggle("open");   
-
-});
